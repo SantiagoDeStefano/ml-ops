4ac9a3d1ab381b833d7ca287a7225d3888820dd0
SantiagoDeStefano <khoinguyenpkn@gmail.com>
2026-03-05 23:41:54 +0700

    It should be working or github is weird

--- scripts/pull_model.py
+++ scripts/pull_model.py
@@ -3,6 +3,7 @@ import os
 
 MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5000")
 MODEL_NAME = os.getenv("MODEL_NAME", "review-sentiment-transformer")
+
 MODEL_VERSION = os.getenv("MODEL_VERSION", "1")
 
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
